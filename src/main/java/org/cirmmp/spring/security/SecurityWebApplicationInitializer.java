84c2655f076a1ea611c94408942843a647d7454e
Andrea <andreagia71@gmail.com>
2017-11-21 16:44:04 +0100

    Created branch XML configuration

--- src/main/java/org/cirmmp/spring/security/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package org.cirmmp.spring.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
